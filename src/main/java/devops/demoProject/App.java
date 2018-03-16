fe95faaec5773a4b92d44161dff7f1195026cdf6
saurabh <gl_saurabh@yahoo.com>
2018-03-16 12:02:19 +0530

    myproject commit

---
+++ src/main/java/devops/demoProject/App.java
@@ -0,0 +1,16 @@
+package devops.demoProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hi" );
+        System.out.println( "bye" );
+        
+    }
+}
